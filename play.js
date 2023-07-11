c1cf4108b2b0fcca30b0825097d84b4da3edccdc
Jerscape <jeremydutton5@gmail.com>
2023-07-11 15:01:21 -0400

    commit play after set up to run game and receive idle message

---
+++ play.js
@@ -0,0 +1,24 @@
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541 //JN - I got this from the error message when I first tried running under port 3000, it seems to work
+  });
+
+
+  conn.on("data", (data) =>{
+    console.log(data)
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
