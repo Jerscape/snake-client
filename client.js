635a71cf4d6b28168d4d997691c9e23129273a76
Jerscape <jeremydutton5@gmail.com>
2023-07-12 11:34:09 -0400

    update after snake initials

---
+++ client.js
@@ -0,0 +1,39 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541 //JN - I got this from the error message when I first tried running under port 3000, it seems to work
+  });
+
+
+  conn.on("data", (data) =>{
+    console.log(data)
+  })
+
+  conn.on("connect", () => {
+    console.log("Sucessfully connected to game server")
+    conn.write("Name: JD")//is thi sin the right place?
+  })
+  
+  // conn.on("write", (data) => {
+  //   console.log("Name: JD")
+  // })
+
+  conn.on("connect", () => {
+    conn.write("Name: JD");
+  });
+
+  
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+
+  return conn;
+};
+
+
+module.exports = {
+  connect
+};
