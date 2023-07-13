a4c35630fb6624c08b0f8ab842ee6bdc3e84b712
Jerscape <jeremydutton5@gmail.com>
2023-07-13 10:17:13 -0400

    update constants file iwnew messages object, and update input file with new code to handle input keys

--- input.js
+++ input.js
@@ -1,5 +1,5 @@
 //imports
-const {commands} = require("./constants")
+const {commands, messages} = require("./constants")
 
 let connection;
 
@@ -14,57 +14,20 @@ const setupInput = function (conn) {
 }
 
 const handleUserInput = function (key) {
+
+  let commandKeys = Object.keys(commands)
+
   if (key === '\u0003') {
     process.exit();
+ 
+  } else if (commandKeys.includes(key)) {
+    let list = commands[key]
+    console.log(list[0]);
+    connection.write(list[1])
+
+  } else {
+    connection.write(messages[key])
   }
-
-  //if key is w
-  if(key === '\u0077'){
-    console.log("w")
-    connection.write("Move: up")
-
-  //if key is a
-  } else if(key === "\u0061") {
-    console.log("a")
-    connection.write("Move: left")
-  //if key is d
-  } else if (key === "\u0064") {
-    console.log("d")
-    connection.write("Move: right")
-  //if key is s
-  } else if(key === "\u0073") {
-    console.log("s")
-    connection.write("Move: down")
-  
-    //special message keys
-    //z "no snakes in Ireland"
-  } else if (key === "\u007A") {
-    connection.write("Say: No snakes in Ireland!")
-    //if key is x
-  } else if (key === '\u0078'){
-    connection.write("Say: Don't eat your tail!")
-  } else if (key === '\u0063'){
-    connection.write("Say: Is it snake or snek?")
-  }
-
-
-  // let keyPressed = key
-  // //should I have made the keys the keys?
-  // let commandObj = {
-  //   w: '\u0077',
-  //   a: '\u0061',
-  //   s: '\u0073',
-  //   d: '\u0064'
-
-  // }
-
-  // for(let item in commandObj){
-  //   if(keyPressed === commandObj[item]){
-  //     //console.log(item)
-  //     connection.write(commandObj[item])
-  //   }
-
-  // }
 
 }
 
