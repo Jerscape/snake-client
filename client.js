429e5ea371c36345b83d549b4df9e9b9ca7a63d9
Jerscape <jeremydutton5@gmail.com>
2023-07-13 09:28:11 -0400

    update after adding constants files, exporting PORT and IP, and amending client file accordingly

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const net = require("net");
 const { stdout } = require("process");
-
+const {IP, PORT} = require("./constants")
+ 
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541 //JN - I got this from the error message when I first tried running under port 3000, it seems to work
+    host: IP,
+    port: PORT, //JN - I got this from the error message when I first tried running under port 3000, it seems to work
   });
 
 
