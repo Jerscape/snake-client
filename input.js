7f7439880f4fadb5c8321e0360b6eb2079aaccdf
Jerscape <jeremydutton5@gmail.com>
2023-07-12 23:28:01 -0400

    snake game controls working

--- input.js
+++ input.js
@@ -1,8 +1,9 @@
 //did I put this in the right place or should it before connect?
 
-let connection = "";
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,22 +17,43 @@ const handleUserInput = function (key) {
     process.exit();
   }
 
-  let keyPressed = key
-  //should I have made the keys the keys?
-  let commandObj = {
-    w: '\u0077',
-    a: '\u0061',
-    s: '\u0073',
-    d: '\u0064'
+  //if key is w
+  if(key === '\u0077'){
+    console.log("w")
+    connection.write("Move: up")
 
+  //if key is a
+  } else if(key === "\u0061") {
+    console.log("a")
+    connection.write("Move: left")
+  //if key is d
+  } else if (key === "\u0064") {
+    console.log("d")
+    connection.write("Move: right")
+  //if key is s
+  } else {
+    console.log("s")
+    connection.write("Move: down")
   }
 
-  for(let item in commandObj){
-    if(keyPressed === commandObj[item]){
-      console.log(item)
-    }
 
-  }
+  // let keyPressed = key
+  // //should I have made the keys the keys?
+  // let commandObj = {
+  //   w: '\u0077',
+  //   a: '\u0061',
+  //   s: '\u0073',
+  //   d: '\u0064'
+
+  // }
+
+  // for(let item in commandObj){
+  //   if(keyPressed === commandObj[item]){
+  //     //console.log(item)
+  //     connection.write(commandObj[item])
+  //   }
+
+  // }
 
 }
 
